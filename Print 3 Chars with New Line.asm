834ae89e4106e1d82c34ebd15f188acf8de40cc9
Nilaycb <nilay.inbox@gmail.com>
2019-04-08 21:01:29 +0600

    Print 3 Chars with New Line code

---
+++ Print 3 Chars with New Line.asm
@@ -0,0 +1,66 @@
+; @author: Nilay Chandra Barman (https://github.com/Nilaycb)
+; Copyright (c) 2019, All rights reserved.
+; Copyrights licensed under the GNU AGPLv3 License.
+
+.MODEL SMALL
+.STACK 100H
+.DATA
+MSG DB "Enter Three initials: $"
+.CODE
+
+MAIN PROC
+    
+    MOV AX,@DATA
+    MOV DS,AX       
+    
+    MOV AH,9H
+    MOV DX,OFFSET MSG       ;PRINT  A
+    INT 21H
+    
+    MOV AH,1
+    INT 21H
+    MOV BL,AL
+    
+    MOV AH,1
+    INT 21H
+    MOV CL,AL
+    
+    MOV AH,1
+    INT 21H
+    MOV DH,AL
+    
+    MOV AH,2
+    MOV DL,0AH
+    INT 21H
+    MOV DL,0DH
+    INT 21H
+    
+    MOV AH,2
+    MOV DL,BL
+    INT 21H
+    
+    MOV AH,2
+    MOV DL,0AH
+    INT 21H
+    MOV DL,0DH
+    INT 21H
+    
+    MOV AH,2
+    MOV DL,CL
+    INT 21H
+    
+    MOV AH,2
+    MOV DL,0AH
+    INT 21H
+    MOV DL,0DH
+    INT 21H
+    
+    MOV AH,2
+    MOV DL,DH
+    INT 21H
+    
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP
+
+END MAIN
